3c505730f604efbd41907bf2998683c15071baac
xhmyjae <tomatis.mt@gmail.com>
2022-09-15 19:51:03 +0200

    feat: add comments to ex01

--- TP01/src/ex01/ex01.java
+++ TP01/src/ex01/ex01.java
@@ -5,6 +5,11 @@ import java.lang.Math;
 
 public class ex01 {
 
+    /**
+     * Calulate square root of inputed number (through getDouble function). If the number is equal to 0 it ends the
+     * program, if the number is negative or too small user will have to input a new one.
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Entrez une valeur positive (0 pour terminer) :");
         double inputDouble = utils.getDouble();
@@ -20,6 +25,7 @@ public class ex01 {
 
         switch ((int)inputDouble) {
             case 0:
+                System.out.println("Fin du programme.");
                 utils.leave(args);
             default:
                 System.out.println("La racine carree de " + inputDouble + " est " + Math.sqrt(inputDouble));
