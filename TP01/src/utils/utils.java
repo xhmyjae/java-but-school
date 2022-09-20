be53ed71ff6101615958ee025d99df00dc8ffa6b
xhmyjae <tomatis.mt@gmail.com>
2022-09-20 23:39:23 +0200

    feat : add comment for ansi values in utils

--- TP01/src/utils/utils.java
+++ TP01/src/utils/utils.java
@@ -163,6 +163,9 @@ public class utils {
         System.out.println();
     }
 
+    /**
+     * Some variables of ANSI values to color text.
+     */
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
