854800696ac030e8fac9973fef7ac9c7f4e2f404
xhmyjae <tomatis.mt@gmail.com>
2022-10-04 11:38:02 +0200

    feat : ex01 done (no comments)

--- TP02/src/ex01/ex01.java
+++ TP02/src/ex01/ex01.java
@@ -1,8 +1,38 @@
 package ex01;
 
+import utils.utils;
+
 public class ex01 {
 
     public static void main(String[] args) {
+        System.out.println("\nEXERCICE 4 :\n");
+
+        int x, y;
+        System.out.print("Entrez le premier nombre, ");
+        do {
+            x = utils.getInt();
+        } while (x < 0);
+        System.out.print("Entrez le deuxieme nombre, ");
+        do {
+            y = utils.getInt();
+        } while (y < 0);
+
+        Archimede cn = new Archimede();
+        System.out.println("Solution a archimede Cn : " + cn.Cn(x, y));
+        System.out.println("Solution a archimede In : " + cn.In(x, y));
+
+        utils.leave(args);
+    }
+
+    public static class Archimede {
+
+        public double Cn(double x, double y) {
+            return 2 * x * y / (x + y);
+        }
+
+        public double In(double x, double y) {
+            return Math.sqrt(x*y);
+        }
 
     }
 
