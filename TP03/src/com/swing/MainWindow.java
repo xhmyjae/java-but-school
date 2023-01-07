3667f4a39ae5ceaf51d6ebbdda43169f681df6e8
xhmyjae <tomatis.mt@gmail.com>
2023-01-07 20:29:11 +0100

    feat(JButtons): fix icon + add messageDialog

--- TP03/src/com/swing/MainWindow.java
+++ TP03/src/com/swing/MainWindow.java
@@ -35,31 +35,9 @@ public class MainWindow {
         menuPanel.setPreferredSize(new Dimension(240,720));
         // Menu buttons
         JButton button1 = new JButton("Etudiants");
-        button1.setPreferredSize(new Dimension(240, 60));
-        button1.setFont(new Font("Bahnschrift", Font.PLAIN, 20));
-        button1.setBackground(Color.decode("#212F45"));
-        button1.setForeground(Color.decode("#7F9AB3"));
-        button1.setFocusable(false);
-        button1.setBorder(BorderFactory.createLineBorder(Color.decode("#212F45")));
-        menuPanel.add(button1);
+        menuButtonDesign(menuPanel, button1);
         JButton button2 = new JButton("JButtons");
-        button2.setPreferredSize(new Dimension(240, 60));
-        button2.setFont(new Font("Bahnschrift", Font.PLAIN, 20));
-        button2.setBackground(Color.decode("#212F45"));
-        button2.setForeground(Color.decode("#7F9AB3"));
-        button2.setFocusable(false);
-        button2.setBorder(BorderFactory.createLineBorder(Color.decode("#212F45")));
-        menuPanel.add(button2);
-//        for (int i = 1; i <= 5; i++) {
-//            JButton button = new JButton("Button " + i);
-//            button.setPreferredSize(new Dimension(240, 50));
-//            button.setFont(new Font("Bahnschrift", Font.PLAIN, 20));
-//            button.setBackground(Color.decode("#212F45"));
-//            button.setForeground(Color.decode("#7F9AB3"));
-//            button.setFocusable(false);
-//            button.setBorder(BorderFactory.createLineBorder(Color.decode("#212F45")));
-//            menuPanel.add(button);
-//        }
+        menuButtonDesign(menuPanel, button2);
         frame.add(menuPanel, BorderLayout.WEST);
 
         // Main panel
@@ -75,13 +53,24 @@ public class MainWindow {
 
         EtudiantsPanel etudiantsPanel = new EtudiantsPanel();
         JButtonsPanel jButtonsPanel = new JButtonsPanel();
+        etudiantsPanel.setBackground(Color.decode("#272640"));
+        jButtonsPanel.setBackground(Color.decode("#272640"));
         mainPanel.add(etudiantsPanel, "etudiantsPanel");
         mainPanel.add(jButtonsPanel, "jButtonsPanel");
-//        mainPanel.add(ex02Panel, "ex02Panel");
 
         // Change main panel on menu buttons click
         button1.addActionListener(e -> cardLayout.show(mainPanel, "etudiantsPanel"));
         button2.addActionListener(e -> cardLayout.show(mainPanel, "jButtonsPanel"));
 
+    }
+
+    private void menuButtonDesign(JPanel menuPanel, JButton button) {
+        button.setPreferredSize(new Dimension(240, 60));
+        button.setFont(new Font("Bahnschrift", Font.PLAIN, 20));
+        button.setBackground(Color.decode("#212F45"));
+        button.setForeground(Color.decode("#7F9AB3"));
+        button.setFocusable(false);
+        button.setBorder(BorderFactory.createLineBorder(Color.decode("#212F45")));
+        menuPanel.add(button);
     }
 }
--- TP03/src/com/swing/jbuttons/JButtonsPanel.java
+++ TP03/src/com/swing/jbuttons/JButtonsPanel.java
@@ -2,6 +2,7 @@ package com.swing.jbuttons;
 
 import javax.swing.*;
 import javax.swing.ImageIcon;
+import java.awt.*;
 
 public class JButtonsPanel extends JPanel {
 
@@ -10,10 +11,50 @@ public class JButtonsPanel extends JPanel {
     }
 
     public void initialize() {
+        // center buttons in panel
+        setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        setPreferredSize(new Dimension(1020, 720));
         JButton button1 = new JButton("Button simple");
-        add(button1);
+        buttonDesign(button1);
+        ImageIcon icon = new ImageIcon(createImageIcon("/resources/icon1.png", "icon1").getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH));
+        JButton button2 = new JButton("Button fantaisie", icon);
+        buttonDesign(button2);
 
-        JButton button2 = new JButton("Button fantaisie", new ImageIcon("resources/icon1.png"));
-        add(button2);
+        button1.addActionListener(e -> {
+            showMessage(this, button1);
+        });
+        button2.addActionListener(e -> {
+            showMessage(this, button2);
+        });
     }
+
+    private void buttonDesign(JButton button) {
+        button.setPreferredSize(new Dimension(230, 60));
+        button.setFont(new Font("Bahnschrift", Font.PLAIN, 20));
+        button.setBackground(Color.decode("#1C1E26"));
+        button.setForeground(Color.decode("#7F9AB3"));
+        button.setFocusable(false);
+        add(button);
+    }
+
+    protected ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    private void showMessage(JPanel panel, JButton button) {
+        if (button.getText().equals("Button fantaisie")) {
+            button.setIcon(new ImageIcon(createImageIcon("/resources/icon2.png", "icon2").getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH)));
+            JOptionPane.showMessageDialog(panel, "Vous avez cliqué sur : " + button.getText());
+            button.setIcon(new ImageIcon(createImageIcon("/resources/icon1.png", "icon1").getImage().getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH)));
+        } else {
+            JOptionPane.showMessageDialog(panel, "Vous avez cliqué sur : " + button.getText());
+        }
+    }
+
 }
